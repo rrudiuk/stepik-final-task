981d512337fe70cdfdd9b1e1876f08602ae258a9
rrudiuk <rus.barik@gmail.com>
2020-07-19 19:36:52 +0300

    Added is_element_presented method to check element presence

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,6 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page = MainPage(browser, link)
@@ -10,3 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
