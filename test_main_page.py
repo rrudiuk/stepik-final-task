ce69650b0bd05f98556a9459b3fd6c3146237776
rrudiuk <rus.barik@gmail.com>
2020-07-25 12:06:58 +0300

    Created locators.py file and login_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,21 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page = MainPage(browser, link)
+    main = MainPage(browser, link)
+    login = LoginPage(browser, link)
     # открываем страницу
-    page.open()
+    main.open()
     # выполняем метод страницы - переходим на страницу логина
-    page.go_to_login_page()
+    main.go_to_login_page()
+    login.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    main = MainPage(browser, link)
+    main.open()
+    main.should_be_login_link()
